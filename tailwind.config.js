ba19c38ffd707a2d2806a0a9c040eb5a9608d692
Changwoo Nam <whoshe@funnyplan.com>
2023-09-08 11:11:35 +0900

    Hero code added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   corePlugins: {
-    preflight: false,
+    preflight: true,
   },
-  content: ['./src/**/*.{html,js}', './public/index.html'],
+  content: [
+    "./src/**/*.{html,js,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          primary: "#37b34a",
+          "primary-focus": "#39D52D",
+        },
+      },
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          primary: "#2b8c3a",
+          "primary-focus": "#37b34a",
+        },
+      },
+    ],
+  },
 };
