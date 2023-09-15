5545dae12eb79d23b25d0389afb6b7de5a640bef
Changwoo Nam <whoshe@funnyplan.com>
2023-09-15 14:06:12 +0900

    Sticky Navbar for mobile updated.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,32 +12,33 @@ module.exports = {
     "./public/**/*.html",
   ],
   theme: {
-    extend: {},
-    fontFamily: {
-      sans: [
-        "Pretendard Variable",
-        "Pretendard",
-        ...defaultTheme.fontFamily.sans,
-      ],
+    extend: {
+      fontFamily: {
+        sans: [
+          "Pretendard Variable",
+          "Pretendard",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
     },
   },
-    plugins: [require("daisyui")],
-    daisyui: {
-      themes: [
-        {
-          light: {
-            ...require("daisyui/src/theming/themes")["[data-theme=light]"],
-            primary: "#37b34a",
-            "primary-focus": "#39D52D",
-          },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          primary: "#37b34a",
+          "primary-focus": "#39D52D",
         },
-        {
-          dark: {
-            ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
-            primary: "#2b8c3a",
-            "primary-focus": "#37b34a",
-          },
+      },
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          primary: "#2b8c3a",
+          "primary-focus": "#37b34a",
         },
-      ],
-    },
-  }
+      },
+    ],
+  },
+};
